86794ba9c54bb2328ebd7c18b90afc51daa448ab
Trần Văn Quyền <90081876+TVQ-FLY@users.noreply.github.com>
2022-03-05 17:38:50 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+var a=prompt("Ten cua ban la: ");
+ var b=prompt("Password:  ");
+  	if(a== "TRAN VAN QUYEN" && b=="K35 DC3")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("Xin loi ! Ban da nhap sai mat khau ");
+  		window.location.href="index.html";
+  	}
+  	
